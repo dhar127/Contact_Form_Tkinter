4f5b5d4e3fa1679d2729003bff2c3311af27650e
DHARANI P <119476559+dhar127@users.noreply.github.com>
2024-07-31 22:52:12 +0530

    Remove all files except Contact book.py

--- QRCODE/qrd.py
+++
@@ -1,6 +0,0 @@
-import qrcode
-import png
-img=qrcode.make("https://www.youtube.com/watch?v=-GmJLI122ZM")
-img.save("k.jpg")
-img=qrcode.make("hello")
-img.sav("l.jpg")
--- pythonProject/kivi.py
+++
@@ -1,3 +0,0 @@
-import qrcode
-generate_image=qrcode.made("DHARANIIIIIIII")
-generate_image.save('image1.png')
